a71cd311004583bc98997968ab8d1ddc7d258557
tygr <workfile975@gmail.com>
2019-08-20 22:37:16 +0800

    ZCR(Zero Cross Rage).

--- NYUST_MIPL_audio_processing/features/feature_extraction.py
+++ NYUST_MIPL_audio_processing/features/feature_extraction.py
@@ -3,3 +3,17 @@ import numpy as np
 def pre_emphasis(x,a=0.95):
     x2= np.append(x[0],x[1:] - a*x[:-1])
     return x2
+
+
+def sgn(val):
+    if val > 0:
+        return 1
+    else:
+        return 0
+
+def zero_crossing_rate(s):
+    zcr = 0 
+    for i in range(1,len(s)):
+        zcr += abs(sgn(s[i]) - sgn(s[i - 1]))
+
+    return zcr
